ded4f6c84cf4222eb8bb80310270010803fdb1e6
krzysztofchadynka <kchadynka@o2.pl>
2014-11-29 17:05:40 +0100

    Initial commit 2
    Add all files

---
+++ administrator/components/com_guestbook1/sql/install.mysql.utf8.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `#_guestbook`
+(
+`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+`message` text NOT NULL,
+`created_by` INTEGER UNSIGNED NOT NULL,
+`location` VARCHAR(45),
+`created` TIMESTAMP NOT NULL,
+`userip` VARCHAR(16),
+PRIMARY KEY(`id`)
+)
---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-10-18 11:56:09 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-10-18T11:56:09+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-18T11:56:15+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-18T11:56:20+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-18T11:56:28+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-11-19T09:34:23+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
